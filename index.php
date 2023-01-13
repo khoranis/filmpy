ac787b4369630fdf60dd10cabfe27df005cb0f07
khoranis <khoranis20@gmail.com>
2023-01-13 23:23:27 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filmpy</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="asset/css/style.css">
+</head>
+<body>
+    <!-- START HEADER -->
+        <header class="header">
+            <div class="profile"></div>
+        </header>
+    <!-- END HEADER -->
+
+
+
+
+
+
+
+
+    <script src="asset/js/script.js"></script>
+</body>
+</html>
